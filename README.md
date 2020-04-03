bac60c805a93ad75eb5cf61b5f5329527d005656
parksion9262@gmail.com <parksion9262@gmail.com>
2020-04-03 21:35:13 +0900

    fastapi user set

---
+++ README.md
@@ -0,0 +1,16 @@
+PYTHON VERSION >= 3.6
+
+pip install fastapi
+
+pip install email-validator
+
+pip install uvicorn
+
+uvicorn main:app --reload
+
+래퍼런스
+
+http://127.0.0.1:8000/docs
+
+
+http://127.0.0.1:8000/redoc
---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import requests
+
+app = FastAPI()
+
+class LoginUser(BaseModel):
+    email : str
+    password : str
+
+class RegisterUser(BaseModel):
+    email : str
+    password : str
+    username : str
+    phonenumber : str
+
+@app.post("/register")
+def register(user:RegisterUser):
+    data = {
+        "email" : user.email,
+        "password" : user.password
+    }
+    result = requests.post('http://localhost:1337/auth/local/register', data=data)
+    print(result)
+    return {
+        "Hello world"
+    }
+
+@app.post("/login")
+def login(user:LoginUser):
+
+    Data = {
+        "identifier" : user.email,
+        "password" : user.password
+    }
+    print(Data)
+    result = requests.post('http://localhost:1337/auth/local', data=Data)
+    print(result)
+
+    return {
+        "Login Sucess"
+    }
